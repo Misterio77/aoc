54a7ab916df5f901363912c950699f953352f0e2
Gabriel Fontes <hi@m7.rs>
2024-09-23 19:34:06 -0300

    feat: add 2015/day1/part2

--- src/2015/day1.hs
+++ src/2015/day1.hs
@@ -1,8 +1,17 @@
 module Main where
 
+parenthesisToInt :: Char -> Int
 parenthesisToInt '(' = 1
 parenthesisToInt ')' = -1
 parenthesisToInt _ = 0
 
-main = print . sum . map parenthesisToInt =<< getContents
+part1 :: String -> Int
+part1 = sum . map parenthesisToInt
 
+part2 :: String -> Int
+part2 = fst . head . filter ((== -1) . snd) . zip [0 ..] . scanl (+) 0 . map parenthesisToInt
+
+main = do
+  input <- getContents
+  print $ part1 input
+  print $ part2 input
