19c6450e9b78f3a6a8696038767e2e2bec3fdbb1
Gabriel Fontes <hi@m7.rs>
2024-12-02 14:54:15 -0300

    wip: 2015 day5

---
+++ src/2015/day5/Main.hs
@@ -0,0 +1,19 @@
+module Main where
+
+vowelCount :: String -> Int
+vowelCount = length . filter (`elem` "aeiou")
+
+hasTwiceLetter :: String -> Bool
+hasTwiceLetter _ = error "TODO"
+
+hasBadStrings :: String -> Bool
+hasBadStrings _ = error "TODO"
+
+isNice :: String -> Bool
+isNice s = vowelCount s >= 3 && hasTwiceLetter s && not (hasBadStrings s)
+
+part1 = isNice
+
+main = do
+  input <- getContents
+  print $ isNice input
