6c951f152870f61d1eb0627fca9d909dc114d66c
Gabriel Fontes <hi@m7.rs>
2024-12-03 18:22:18 -0300

    y2024-d3: simplify

--- src/2024/day3/Instruction.hs
+++ src/2024/day3/Instruction.hs
@@ -1,21 +1,21 @@
 module Instruction where
 
 import Data.Maybe (mapMaybe)
-import Text.Regex.TDFA (getAllTextMatches, getAllTextSubmatches, (=~))
+import Text.Regex.TDFA ((=~))
 
 data Instruction = Do | Dont | Mul (Int, Int) deriving (Show)
 
 fromString :: String -> Bool -> [Instruction]
-fromString input conditionalStatements = map parse (input =~ pattern)
+fromString input conditionalStatements = map parse $ input =~ pattern
   where
     pattern =
       if conditionalStatements
-        then "(mul)\\(([0-9]{1,3}),([0-9]{1,3})\\)|(don't)\\(\\)|(do)\\(\\)"
-        else "(mul)\\(([0-9]{1,3}),([0-9]{1,3})\\)"
+        then "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|don't\\(\\)|do\\(\\)"
+        else "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"
     parse :: [String] -> Instruction
-    parse (_ : "mul" : x : y : _) = Mul (read x, read y)
-    parse (_ : _ : _ : _ : "don't" : _) = Dont
-    parse (_ : _ : _ : _ : _ : "do" : _) = Do
+    parse ("do()" : _) = Do
+    parse ("don't()" : _) = Dont
+    parse (_ : x : y : _) = Mul (read x, read y)
 
 execute :: [Instruction] -> Int
 execute xs = execute' xs True
